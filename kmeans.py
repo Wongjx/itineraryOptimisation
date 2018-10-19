e3cbedbb324d01b892b6e609da9a4b5bc46c0072
Wong Jun Xiang  (NCS) <junxiang.wong@ncs.com.sg>
2018-10-19 16:41:11 +0800

    kmeans.py refactor

--- kmeans.py
+++ kmeans.py
@@ -5,29 +5,34 @@ from processKML import parseKMLforKmeans
 from sklearn.cluster import KMeans
 
 
-# placemarks = parseKML("doc.KML")
-nameList,comList = parseKMLforKmeans("doc.KML")
-comListArray = np.array(comList)
+def kmeansKML(nameList,comList,showPlot=False):
+    # X, y_true = make_blobs(n_samples=50, centers=4,cluster_std=0.60, random_state=0)
+    # plt.scatter(X[:, 0], X[:, 1], s=50)
 
-# X, y_true = make_blobs(n_samples=50, centers=4,cluster_std=0.60, random_state=0)
-# plt.scatter(X[:, 0], X[:, 1], s=50)
+    comListArray = np.array(comList)
+    kmeans = KMeans(n_clusters=4)
+    kmeans.fit(comListArray)
+    y_kmeans = kmeans.predict(comListArray)
+    centers = kmeans.cluster_centers_
+    if(showPlot):
+        plt.scatter(comListArray[:, 0], comListArray[:, 1], c=y_kmeans, s=50, cmap='viridis')
+        plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
+        plt.show()
+    return y_kmeans
 
-kmeans = KMeans(n_clusters=4)
-kmeans.fit(comListArray)
-y_kmeans = kmeans.predict(comListArray)
-centers = kmeans.cluster_centers_
+def saveToCsv(file,nameList,y_kmeans):
+    with open("output.csv",'w',encoding='utf8') as csvfile:
+        for i in range(len(nameList)):
+            line = nameList[i]+","+str(y_kmeans[i])+'\n'
+            # csvfile.write(line.encode('utf8'))
+            csvfile.write(line)
 
-# plt.scatter(comListArray[:, 0], comListArray[:, 1], c=y_kmeans, s=50, cmap='viridis')
-# plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
-# plt.show()
+if __name__ == "__main__":
+    outFile =  "output.csv"
+    inFile = "doc.KML"
 
-print(y_kmeans)
-print(y_kmeans.shape)
-with open("output.csv",'w',encoding='utf8') as csvfile:
-    for i in range(len(nameList)):
-        line = nameList[i]+","+str(y_kmeans[i])+'\n'
-        # csvfile.write(line.encode('utf8'))
-        csvfile.write(line)
-
+    nameList,comList = parseKMLforKmeans(inFile)
+    y_kmeans = kmeansKML(nameList,comList)
+    saveToCsv(outFile,nameList,y_kmeans)
 
 
